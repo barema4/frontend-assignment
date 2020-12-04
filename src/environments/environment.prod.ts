5795229df1ddbec56d21b4fc6bb1a2305b9d8e72
barema4 <samrubarema6@gmail.com>
2020-12-04 20:17:51 +0300

    add initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCGQ6CTXnouxTYtCmL9qii6w-xoxtsnkhE',
+    authDomain: 'fir-authentication-327cb.firebaseapp.com',
+    databaseURL: 'https://fir-authentication-327cb.firebaseio.com',
+    projectId: 'fir-authentication-327cb',
+    storageBucket: 'fir-authentication-327cb.appspot.com',
+    messagingSenderId: '306350701930',
+    appId: '1:306350701930:web:0e29a12bbde4ebc5c37913',
+    measurementId: 'G-5BBPH45DEB'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCGQ6CTXnouxTYtCmL9qii6w-xoxtsnkhE',
+    authDomain: 'fir-authentication-327cb.firebaseapp.com',
+    databaseURL: 'https://fir-authentication-327cb.firebaseio.com',
+    projectId: 'fir-authentication-327cb',
+    storageBucket: 'fir-authentication-327cb.appspot.com',
+    messagingSenderId: '306350701930',
+    appId: '1:306350701930:web:0e29a12bbde4ebc5c37913',
+    measurementId: 'G-5BBPH45DEB'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
